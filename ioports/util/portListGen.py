708cb95618fc9867feb127af7b9fe616d1b10b21
Rory M <rory.mackie@arcturussecurity.com>
2019-11-08 15:07:50 +0000

    Updates, directory tidy, new bitflip test code
    ./bitflip/
        - Added some bit/byte flipping library code
    ./ioports/
        - New directory for all io port fuzzing stuff
    ./ioports/util/portListGen.py
        - Finished port list generator
    ./ioports/Makefile
        - Added 'portlist' command to Makefile
    ./ioports/iof.c
        - Modified fuzzer to use generated portlist file

--- ioports/util/portListGen.py
+++ ioports/util/portListGen.py
@@ -1,19 +1,22 @@
 #!/usr/bin/env python3
-import sys
-
-# WIP
+from sys import stdin
 
 # pipe to this from 
 # cat /proc/ioports|sed -s 's/ //g'|sed -s 's/:.*//g'|sort|uniq
-portListRaw = sys.stdin.read()
+portListRaw = stdin.read()
 portList = portListRaw.split("\n")
 
 # Get rid of blank entry at end of file
 if "" in portList: portList.pop(portList.index(""))
 
-# How do we deal with buses?
+PORTS = []
 for RANGE in portList:
 	start,end = RANGE.split("-")
-	start = hex(int("0x0"+start,16))
-	end = hex(int("0x0"+end,16))
-	print(start, "to", end)
+	start = int("0x0"+start,16)
+	end = int("0x0"+end,16)
+	for Q in range(end-start):
+		PORTS += [str(hex(Q+start)), ]
+
+PORTLIST = f"uint16_t portList[] = {{{','.join(PORTS)}}}\n#define PORTLISTLEN {len(PORTS)}\n"
+
+print(PORTLIST)
