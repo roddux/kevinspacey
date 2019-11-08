393802c839bbfc59202dbc1d0a8259b1c18a4143
Rory M <rory.mackie@arcturussecurity.com>
2019-11-08 15:29:58 +0000

    Name updates, comments, ready to add fuzz hooks
    ./kmod/newmod.c
        - Added code comments, explaining thought process
        - Renamed module to 'kvsp' instead of 'hackmod'
        - Added taskfile struct info ready to fuzz

--- kmod/newmod.c
+++ kmod/newmod.c
@@ -1,19 +1,35 @@
 #include <linux/module.h>
-#include "fopskit.h"
-
 #include <linux/libata.h>
+#include "fopskit.h"
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("roddux");
-MODULE_DESCRIPTION("hackmod");
+MODULE_DESCRIPTION("kvsp");
 
+// Most common commands
 #define ATA_CMD_FLUSH 0xE7
 #define ATA_CMD_FPDMA_READ 0x60
 #define ATA_CMD_FPDMA_WRITE 0x61
 #define ATA_CMD_PACKET 0xA0
 
+// ata_qc_issue, in our case, calls ahci_qc_issue
+// that's what we wanna look at for hardware operations
+// also ahci_qc_prep, ahci_fill_cmd_slot ...
+
+// amusingly, ahci_qc_issue takes exactly the same input as ata_qc_issue
+// which means we can short-circuit ata_qc_issue
+// just use our hook to fuzz the ata_queued_cmd struct, then
+// return directly to ahci_qc_issue.
+
+void fuzz(void *data) {
+	// we know sizeof() is sizeof(ata_queued_cmd), so we can cheat
+	// seek to a random offset using getrandom()
+	// call getrandom() passing *data as an argument, overwriting a few bits
+}
+
 fopskit_hook_handler(ata_qc_issue) {
-//	printk("ftr: ata_qc_issue");
+//	This hook runs before the main ata_qc_issue code does
+//	Which means we can fuck with all it's arguments
 
 	struct ata_queued_cmd *CMD = REGS_ARG1;
 //	printk("ata_queued_cmd: %X", &CMD);
@@ -22,35 +38,46 @@ fopskit_hook_handler(ata_qc_issue) {
 
 	switch (TF->command) {
 		case ATA_CMD_FLUSH:
-			printk("ftr: ata_qc_issue cmd %X (ATA_CMD_FLUSH)",TF->command);
+			printk("kvsp: ata_qc_issue cmd %X (ATA_CMD_FLUSH)",TF->command);
+			break;
+		case ATA_CMD_FPDMA_READ:
+			printk("kvsp: ata_qc_issue cmd %X (ATA_CMD_FPDMA_READ)",TF->command);
 			break;
 		case ATA_CMD_FPDMA_WRITE:
-			printk("ftr: ata_qc_issue cmd %X (ATA_CMD_FPDMA_WRITE)",TF->command);
+			printk("kvsp: ata_qc_issue cmd %X (ATA_CMD_FPDMA_WRITE)",TF->command);
 			break;
 		case ATA_CMD_PACKET:
-			printk("ftr: ata_qc_issue cmd %X (ATA_CMD_PACKET)",TF->command);
+			printk("kvsp: ata_qc_issue cmd %X (ATA_CMD_PACKET)",TF->command);
 			break;
 		default:
-			printk("ftr: ata_qc_issue cmd %X (not defined)",TF->command);
+			printk("kvsp: ata_qc_issue cmd %X (not defined)",TF->command);
 			break;
 	}
 
+	struct ata_queued_cmd CMDBU;
+	CMDBU = CMD;
+	for (i=0;i<100;i++) {
+		fuzz(&CMD);
+		ahci_qc_issue(CMD);
+	}
+	// TF->command = ATA_CMD_FPDMA_READ;
+	// this kills the crab^H^H^H^Hvm
 }
 
-
+// TODO: Remove the fopskit helper functions and use ftrace directly (?)
 struct fops_hook hook_ata_qc_issue = fops_hook_val(ata_qc_issue);
 
 static int __init test_init(void) {
-	printk("ftr: hooking...");
+	printk("kvsp: hooking...");
 	fopskit_sym_hook(&hook_ata_qc_issue);
-	printk("ftr: okay!");
+	printk("kvsp: okay!");
 	return 0;
 }
 
 static void __exit test_exit(void) {
-	printk("ftr: unhooking...");
+	printk("kvsp: unhooking...");
 	fopskit_sym_unhook(&hook_ata_qc_issue);
-	printk("ftr: bye!");
+	printk("kvsp: bye!");
 }
 
 module_init(test_init);
